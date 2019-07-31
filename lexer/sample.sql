af5ec4136b3071237480eed49daae7473dfd7a83
denton <alexander.rybakov@gmail.com>
2019-07-31 18:08:03 +0300

    fixed test sql

--- lexer/sample.sql
+++ lexer/sample.sql
@@ -1,39 +1,31 @@
-SELECT 
-    field1 as f1,
+SELECT
+    s1.field1 as f1,
     field2 f2,
     distinct field3,
-    (field4*5)/6.0-7%8\3+5.93,
+    (field4*5)/6.0-7%8\3+5.93+s3.field2 f4,
     count(*),
     avg(field1) OVER(PARTITION By field2 order by filed3 desc rows between UNBOUNDED PRECEDING and UNBOUNDED FOLLOWING)
+    userfunction(f1)
+from
+    ~/dir/file1.xlsx#sheet1 as s1,
+    ../dir2/file2.csv s2,
+    left outer join ./file3.csv s3 on s3.f1 = s2.f1 and  s3 = 4
 window 
 into
 
 Where
-    f1
-having
-union
-gro
-5 as d, 
-d,
-distinct l,
-count(*),
-sum(l),
-max(4),
-min(distinct t) as t,
-ROW_NUMBER() OVER(PARTITION BY c
-ORDER BY OrderAmount DESC) AS "Row Number", 
-CONVERT(VARCHAR(20), SUM(o) OVER(PARTITION BY c ORDER BY OrderAmount DESC ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING), 1) AS CumulativeTotal,
-from ./data_log.xlsx 
-inner join data.csv on m.id = data.m_id AND s = 1
-outer left right join s
-Where x = and 1 = 1 or data in (3,4,5)
+    f1 > 4
+    and (s1.f3 > '2019-01-02' or s2.f4 <> 4)
+    and f4 is not null
+    or s1.f1 in (1,2,3,4,5)
 group by
-a, b, s
+
 sort by
-a asc,
-b,
-d desc
-c
-union 
-select q.d, 5,6,4, count(3) 
-from {select * from file.xlsx#sheer1
+    field1,
+    field2 asc,
+    field3 desc
+having
+    f1 = true
+    and f2 <> false
+
+union
